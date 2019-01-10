2ce147aa9dcbebe85fa90cccb2c53c6289b2b65a
milktegi <milkteegi@gmail.com>
2019-01-10 10:30:52 +0900

    Rendered CardList

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'andrei',
+    username: 'bret',
+    email: 'sincere@jan.com'
+  },
+  {
+    id: 2,
+    name: 'jhop',
+    username: 'bts007',
+    email: 'idol@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'wheein',
+    username: 'kiyomi',
+    email: 'lovelove@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'coldbrew',
+    username: 'iceshot',
+    email: 'espresso@starbucks.com'
+  },
+  {
+    id: 5,
+    name: 'juyeon',
+    username: 'devrookie',
+    email: 'rookie@junior-dev.com'
+  },
+  {
+    id: 6,
+    name: 'fillingood',
+    username: 'jazzy',
+    email: 'sappy@dazzed.com'
+  },
+  {
+    id: 7,
+    name: 'fingerprice',
+    username: 'gitbash',
+    email: 'halloo@bmail.com'
+  },
+  {
+    id: 8,
+    name: 'labor',
+    username: 'sucks',
+    email: 'butiwantmoney@hireme.com'
+  },
+  {
+    id: 9,
+    name: 'playtennis',
+    username: 'wouldbfun',
+    email: 'becomebarista@coffeebean.com'
+  },
+  {
+    id: 10,
+    name: 'perfect',
+    username: 'awesome',
+    email: 'ph-1@goodmusic.com'
+  }
+];
