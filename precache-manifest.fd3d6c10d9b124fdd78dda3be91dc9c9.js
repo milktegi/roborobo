de0ddc81fc4d9b62dcc7f4c9c1e4faed4efcf486
milktegi <milkteegi@gmail.com>
2019-01-10 13:52:20 +0900

    Updates

---
+++ precache-manifest.fd3d6c10d9b124fdd78dda3be91dc9c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roborobo/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "30d6e1acf4c5f0c487a5",
+    "url": "/roborobo/static/js/runtime~main.30d6e1ac.js"
+  },
+  {
+    "revision": "2f42421c97b46e399cc1",
+    "url": "/roborobo/static/js/main.2f42421c.chunk.js"
+  },
+  {
+    "revision": "eaf0b7cb26e197810c7c",
+    "url": "/roborobo/static/js/1.eaf0b7cb.chunk.js"
+  },
+  {
+    "revision": "2f42421c97b46e399cc1",
+    "url": "/roborobo/static/css/main.0cb29c86.chunk.css"
+  },
+  {
+    "revision": "eaf0b7cb26e197810c7c",
+    "url": "/roborobo/static/css/1.5266ef7e.chunk.css"
+  },
+  {
+    "revision": "74f73565676ba578afd61a7df1e2142b",
+    "url": "/roborobo/index.html"
+  }
+];
